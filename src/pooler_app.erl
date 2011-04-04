4804b3ed8789fbe63aa46a1238ea17703949c0f3
Seth Falcon <seth@userprimary.net>
2011-04-03 22:48:09 -0700

    Start crypto in app startup

--- src/pooler_app.erl
+++ src/pooler_app.erl
@@ -10,6 +10,7 @@
 %% ===================================================================
 
 start(_StartType, _StartArgs) ->
+    application:start(crypto),
     case pooler_sup:start_link() of
         {ok, Pid} -> {ok, Pid};
         Other -> {error, Other}
