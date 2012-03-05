50e35552d58f7fdad6456015e673ab09d611e948
Seth Falcon <seth@userprimary.net>
2012-03-04 22:09:07 -0800

    Declare crypto as a dependency in pooler.app, don't start it
    Let OTP do its thing.

--- src/pooler_app.erl
+++ src/pooler_app.erl
@@ -10,7 +10,6 @@
 %% ===================================================================
 
 start(_StartType, _StartArgs) ->
-    application:start(crypto),
     case pooler_sup:start_link() of
         {ok, Pid} -> {ok, Pid};
         Other -> {error, Other}
