7b1bcacaa3104bc2c03be8eaf448abbc0b572504
drewkerrigan <dkerrigan@basho.com>
2015-03-05 17:11:30 -0700

    Prepping for release to Hex

---
+++ package.exs
@@ -0,0 +1,19 @@
+defmodule Pooler.Mixfile do
+  use Mix.Project
+
+  @version File.read!("VERSION") |> String.strip
+
+  def project do
+    [app: :poolboy,
+     version: @version,
+     description: "An OTP Process Pool Application",
+     package: package]
+  end
+
+  defp package do
+    [files: ~w(src rebar.config rebar.config.script Makefile concrete.mk README.org LICENSE VERSION),
+     contributors: ["Seth Falcon"],
+     licenses: ["Apache 2.0"],
+     links: %{"GitHub" => "https://github.com/seth/pooler"}]
+  end
+end
